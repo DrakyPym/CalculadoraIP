2137126fb0d2872eb4ea202d3c484be5ff86a9e3
DrakyPym <darkericorbit.020802@gmail.com>
2023-11-24 23:47:23 -0600

    Mas funcionalidades

--- CalculadoraIP.py
+++ CalculadoraIP.py
@@ -5,15 +5,20 @@ def obtener_informacion_red(direccion_ip, mascara):
         # Crear un objeto de red utilizando ip_network
         red = ipaddress.ip_network(f"{direccion_ip}/{mascara}", strict=False)
 
-        print(f"Información para la red {red}:")
+        print(f"\nInformación para la red {red}:\n")
         print(f"Dirección de red: {red.network_address}")
         print(f"Máscara de red: {red.netmask}")
+        print(f"Wilcard: {red.hostmask}")
         print(f"Cantidad de hosts en la red: {len(list(red.hosts()))}")
+        hosts = list(red.hosts())
+        print(f"Host mínimo: {hosts[0]}")
+        print(f"Host máximo: {hosts[-1]}")
+        print(f"Dirección de Broadcast: {red.broadcast_address}")
         
     except ValueError:
         print("¡Dirección IP o máscara de red no válida!")
 
 direccion_ip_usuario = input("Ingrese una dirección IP: ")
-mascara_usuario = input("Ingrese la máscara de red: \n")
+mascara_usuario = input("Ingrese la máscara de red: ")
 
 obtener_informacion_red(direccion_ip_usuario, mascara_usuario)
