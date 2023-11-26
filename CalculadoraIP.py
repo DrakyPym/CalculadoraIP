afb47dd734f66481053328bcc7c5e888479c977a
DrakyPym <darkericorbit.020802@gmail.com>
2023-11-25 18:52:10 -0600

    Control de errores

--- CalculadoraIP.py
+++ CalculadoraIP.py
@@ -14,25 +14,27 @@ def obtener_informacion_red(direccion_ip, mascara):
         print(f"Host mínimo: {hosts[0]}")
         print(f"Host máximo: {hosts[-1]}")
         print(f"Dirección de Broadcast: {red.broadcast_address}")
-        
     except ValueError:
         print("¡Dirección IP o máscara de red no válida!")
         
 def obtener_subredes(direccion_ip, mascara, nueva_mascara):
-    # Crear un objeto de red utilizando ip_network
-    red = ipaddress.ip_network(f"{direccion_ip}/{mascara}", strict=False)
-    print("###################################################")
-    print(f"\nSubredes con la nueva máscara (/{nueva_mascara}):")
-    for subred in red.subnets(prefixlen_diff=(int(nueva_mascara) - int(mascara))):
-        print(f"\nInformación para la subred {subred}:\n")
-        print(f"Dirección de subred: {subred.network_address}")
-        print(f"Máscara de subred: {subred.netmask}")
-        print(f"Wilcard: {subred.hostmask}")
-        print(f"Cantidad de hosts en la subred: {len(list(subred.hosts()))}")
-        hosts = list(subred.hosts())
-        print(f"Host mínimo: {hosts[0]}")
-        print(f"Host máximo: {hosts[-1]}")
-        print(f"Dirección de Broadcast: {subred.broadcast_address}")
+    try:    
+        # Crear un objeto de red utilizando ip_network
+        red = ipaddress.ip_network(f"{direccion_ip}/{mascara}", strict=False)
+        print("###################################################")
+        print(f"\nSubredes con la nueva máscara (/{nueva_mascara}):")
+        for subred in red.subnets(prefixlen_diff=(int(nueva_mascara) - int(mascara))):
+            print(f"\nInformación para la subred {subred}:\n")
+            print(f"Dirección de subred: {subred.network_address}")
+            print(f"Máscara de subred: {subred.netmask}")
+            print(f"Wilcard: {subred.hostmask}")
+            print(f"Cantidad de hosts en la subred: {len(list(subred.hosts()))}")
+            hosts = list(subred.hosts())
+            print(f"Host mínimo: {hosts[0]}")
+            print(f"Host máximo: {hosts[-1]}")
+            print(f"Dirección de Broadcast: {subred.broadcast_address}")
+    except ValueError:
+        print("¡Dirección IP o máscara de red no válida!")
 
 def decimal_a_binario(decimal):
     if decimal < 0:
